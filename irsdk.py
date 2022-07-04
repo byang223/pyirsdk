2cbda234e59958e1d85a3aa3d9f22d76eca20eb8
Bryan Yang <byang223@gmail.com>
2022-07-04 15:20:11 -0700

    Ir telemetry collector (#1)
    * add parse_to_json capabilities
    
    * uploader: create uploader
    
    * add requirements and test data
    
    * uploader: fix minor issue with parser
    
    * irsdk: minor fix to get_json method

--- irsdk.py
+++ irsdk.py
@@ -6,6 +6,7 @@ import mmap
 import struct
 import ctypes
 import yaml
+import json
 from threading import Thread
 from urllib import request, error
 from yaml.reader import Reader as YamlReader
@@ -447,6 +448,28 @@ class IRSDK:
         ]))
         f.close()
 
+    def to_dict(self):
+      results = {}
+      for i in sorted(self._var_headers_dict.keys(), key=str.lower):
+        results[i] = self[i]
+      return results
+
+    def get_json(self):
+      session_yml = self._shared_mem[self._header.session_info_offset:self._header.session_info_len].rstrip(b'\x00').decode(YAML_CODE_PAGE)
+      telemetry_json = json.dumps(self.to_dict(), indent=2)
+      return {
+        "Static": yaml.load(session_yml, Loader=CustomYamlSafeLoader),
+        "Telemetry": self.to_dict(),
+      }
+    
+    def parse_to_json(self, to_file):
+        if not self.is_initialized:
+            return
+        f = open(to_file, 'w', encoding='utf-8')
+        f = open(to_file, 'w', encoding='utf-8')
+        json.dump(self.get_json(), f, indent=2)
+        f.close()
+
     def cam_switch_pos(self, position=0, group=1, camera=0):
         return self._broadcast_msg(BroadcastMsg.cam_switch_pos, position, group, camera)
 
@@ -768,7 +791,7 @@ def main():
     ir.startup(test_file=args.test, dump_to=args.dump)
 
     if args.parse:
-        ir.parse_to(args.parse)
+        ir.parse_to_json(args.parse)
 
 if __name__ == '__main__':
     main()
---
+++ uploader.py
@@ -0,0 +1,58 @@
+#!python3
+
+import argparse
+import time
+import requests
+from irsdk import IRSDK
+
+# this is our State class, with some helpful variables
+class State:
+  ir_connected = False
+  last_car_setup_tick = -1
+
+# here we check if we are connected to iracing
+# so we can retrieve some data
+def check_iracing():
+  if state.ir_connected and not (ir.is_initialized and ir.is_connected):
+    state.ir_connected = False
+    # don't forget to reset your State variables
+    state.last_car_setup_tick = -1
+    # we are shutting down ir library (clearing all internal variables)
+    ir.shutdown()
+    print('irsdk disconnected')
+  elif not state.ir_connected and ir.startup() and ir.is_initialized and ir.is_connected:
+    state.ir_connected = True
+    print('irsdk connected')
+
+# our main loop, where we retrieve data
+# and do something useful with it
+def loop(ir, url):
+  ir.freeze_var_buffer_latest()
+  telemetry = ir.get_json()
+  requests.post(url, json = telemetry)
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--url', help='url of the api to post telemetry to', required=True)
+
+  args = parser.parse_args()
+
+  # initializing ir and state
+  ir = IRSDK()
+  state = State()
+
+  try:
+    # infinite loop
+    while True:
+      # check if we are connected to iracing
+      check_iracing()
+      # if we are, then process data
+      if state.ir_connected:
+        loop(ir, args.url)
+      # sleep for 1 second
+      # maximum you can use is 1/60
+      # cause iracing updates data with 60 fps
+      time.sleep(1)
+  except KeyboardInterrupt:
+    # press ctrl+c to exit
+    pass
